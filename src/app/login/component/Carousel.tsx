8dc3f57bc45318562094730cdad52d094b8d6e1e
VDBay <azharsubhan97@gmail.com>
2025-04-08 21:30:36 +0700

    :lipstick: add many ui

---
+++ src/app/login/component/Carousel.tsx
@@ -0,0 +1,9 @@
+export default function Carousel() {
+  return (
+    <div className="flex gap-18 items-center pb-6">
+      <div className="bg-gray-300 aspect-square h-60 "></div>
+      <div className="bg-gray-300 aspect-square h-80"></div>
+      <div className="bg-gray-300 aspect-square h-60"></div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,22 @@
 "use client";
 import { Image } from "@mui/icons-material";
 import Button from "@mui/material/Button";
+import Navbar from "./components/Navbar";
+import Hero from "./login/component/Hero";
+import Carousel from "./login/component/Carousel";
 
 export default function HomePage() {
   return (
     <div className="flex min-h-screen bg-white flex-col items-center">
-      <div className="w-full flex items-center justify-between text-black h-18 px-16">
-        <Button color="inherit">HOME</Button>
-        <Image className="absolute left-1/2 -translate-x-1/2 w-full h-full object-cover rounded-2xl" />
-        <Button color="inherit">REGISTER</Button>
-      </div>
-      <div
-        className="bg-gray-300 w-full"
-        style={{ height: "calc(100svh - 72px)" }}
-      ></div>
+      <Navbar />
+      <Hero />
       <div className="flex text-2xl p-18">Your Glow Starts Here</div>
-      <div className="flex gap-18 items-center pb-6">
-        <div className="bg-gray-300 aspect-4/5 h-76 "></div>
-        <div className="bg-gray-300 aspect-4/5 h-90"></div>
-        <div className="bg-gray-300 aspect-4/5 h-76"></div>
-      </div>
+      <Carousel />
       <div className="font-medium text-base pb-3">Product Name</div>
-      <div className="text-sm font-normal w-70 text-center  pb-6">
+      <div className="text-base font-normal w-90 text-center pb-3 text-gray-600">
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
       </div>
-      <div className="text-lg font-semibold pb-4">Rp XXX.XXX</div>
+      <div className="text-base font-semibold pb-6">Rp XXX.XXX</div>
       <div className="pb-18">
         {" "}
         <Button
@@ -43,6 +35,82 @@ export default function HomePage() {
           BUY NOW
         </Button>
       </div>
+      <div className="flex w-full h-svh">
+        <div className="bg-gray-300 w-1/2 h-full"></div>
+        <div className="bg-gray-400 w-1/2 h-full"></div>
+      </div>
+      <div className="flex text-2xl p-18">Special Offer</div>
+      <div className="flex gap-18 items-center pb-18">
+        <div className="flex flex-col items-center gap-2">
+          <div className="bg-gray-300 aspect-square h-80 mb-4"></div>
+          <div className="font-medium text-base ">Product Name</div>
+          <div className="text-base font-normal w-90 text-center pb-3 text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </div>
+          <div className="text-base font-semibold pb-6">Rp XXX.XXX</div>
+        </div>
+        <div className="flex flex-col items-center gap-2">
+          <div className="bg-gray-300 aspect-square h-80 mb-4"></div>
+          <div className="font-medium text-base ">Product Name</div>
+          <div className="text-base font-normal w-90 text-center pb-3 text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </div>
+          <div className="text-base font-semibold pb-6">Rp XXX.XXX</div>
+        </div>
+        <div className="flex flex-col items-center gap-2">
+          <div className="bg-gray-300 aspect-square h-80 mb-4"></div>
+          <div className="font-medium text-base ">Product Name</div>
+          <div className="text-base font-normal w-90 text-center pb-3 text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </div>
+          <div className="text-base font-semibold pb-6">Rp XXX.XXX</div>
+        </div>
+      </div>
+      <div className="flex gap-18 items-center pb-20">
+        <div className="flex flex-col items-center gap-2">
+          <div className="bg-gray-300 aspect-square h-80 mb-4"></div>
+          <div className="font-medium text-base ">Product Name</div>
+          <div className="text-base font-normal w-90 text-center pb-3 text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </div>
+          <div className="text-base font-semibold pb-6">Rp XXX.XXX</div>
+        </div>
+        <div className="flex flex-col items-center gap-2">
+          <div className="bg-gray-300 aspect-square h-80 mb-4"></div>
+          <div className="font-medium text-base ">Product Name</div>
+          <div className="text-base font-normal w-90 text-center pb-3 text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </div>
+          <div className="text-base font-semibold pb-6">Rp XXX.XXX</div>
+        </div>
+        <div className="flex flex-col items-center gap-2">
+          <div className="bg-gray-300 aspect-square h-80 mb-4"></div>
+          <div className="font-medium text-base ">Product Name</div>
+          <div className="text-base font-normal w-90 text-center pb-3 text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </div>
+          <div className="text-base font-semibold pb-6">Rp XXX.XXX</div>
+        </div>
+      </div>
+      <div className="pb-18">
+        {" "}
+        <Button
+          variant="contained"
+          sx={{
+            borderRadius: 0,
+            backgroundColor: "black",
+            color: "white",
+            "&:hover": {
+              backgroundColor: "#333",
+            },
+            px: 8,
+            py: 2,
+          }}
+        >
+          VIEW MORE
+        </Button>
+      </div>
+      <div className="bg-gray-300 aspect-239/100 w-full mb-18"></div>
     </div>
   );
 }
