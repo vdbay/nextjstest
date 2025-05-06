e3fbd62e93cd69da89a4e810d9bf95f1e4186759
VDBay <azharsubhan97@gmail.com>
2025-05-06 14:02:56 +0700

    :sparkles: re

--- src/component/global/navigationbar/NavigationBarMenu.tsx
+++
@@ -1,9 +0,0 @@
-export default function NavigationBarMenu() {
-  return (
-    <div className="gap-10 hidden md:flex flex-row ">
-      <div>Home</div>
-      <div>Product</div>
-      <div>About Us</div>
-    </div>
-  );
-}
--- src/component/global/navigationbar/NavigationBarRightMenu.tsx
+++
@@ -1,8 +0,0 @@
-export default function NavigationBarRightMenu() {
-  return (
-    <div className="gap-10 hidden md:flex flex-row ">
-      <div>Login</div>
-      <div>Cart</div>
-    </div>
-  );
-}
