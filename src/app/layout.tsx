d19adfacbb4caf5bc6003cd23b6949c6bb37f616
VDBay <azharsubhan97@gmail.com>
2025-05-14 21:46:25 +0700

    :sparkles: add speed insight vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import NavigationBar from "@/components/layout/navigationbar";
 import Footer from "@/components/layout/footer";
 
@@ -47,6 +48,7 @@ export default function RootLayout({
         suppressHydrationWarning
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
