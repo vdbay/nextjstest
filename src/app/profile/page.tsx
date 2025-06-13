e86fcdea5e0c6689bc443256e5e622083cd8bf6a
VDBay <azharsubhan97@gmail.com>
2025-06-13 08:44:59 +0700

    :sparkles: using bun

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,12 @@
+import Footer from "@/components/layout/footer";
+import NavigationBar from "@/components/layout/navigationbar";
+
+export default function Profile() {
+  return (
+    <>
+      <NavigationBar />
+      <div className="min-h-svh"></div>
+      <Footer />
+    </>
+  );
+}
